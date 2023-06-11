12db5952128da5fa52397d01d2c438be07655141
luthfipratamaps <luthfipratamaps@gmail.com>
2023-06-11 19:59:26 +0700

    modified generate data to no gps data

--- init_data/generateDataDb.py
+++ init_data/generateDataDb.py
@@ -20,13 +20,10 @@ current_time = start_time
 
 today = datetime.now()
 days = []
-for i in range(7):
+for i in range(7, 0, -1):
     date = today - timedelta(days=i)
     date_string = date.strftime('%Y-%m-%d')
     days.append(date_string)
-
-Lats = [-6.9174639, -6.9174640, -6.9174641, -6.9174642]
-Longs = [107.6191228, 107.6191217, 107.6191206, 107.6191195]
 
 for day in days:
     while current_time <= end_time:
@@ -47,22 +44,14 @@ for day in days:
         IC2 = round(random.uniform(100, 1000), 2)
         IC3 = round(random.uniform(100, 1000), 2)
         IC4 = round(random.uniform(100, 1000), 2)
-        Latitude1 = Lats[0]
-        Latitude2 = Lats[1]
-        Latitude3 = Lats[2]
-        Latitude4 = Lats[3]
-        Longitude1 = Longs[0]
-        Longitude2 = Longs[1]
-        Longitude3 = Longs[2]
-        Longitude4 = Longs[3]
         
         # Format the date and time strings
         date_string = day
         time_string = current_time.strftime('%H:%M:%S')
 
         # Insert the data into the table
-        sql = "INSERT INTO monitoring_data (Tanggal, Waktu, Suhu1, Suhu2, Suhu3, Suhu4, RH1, RH2, RH3, RH4, SH1, SH2, SH3, SH4, IC1, IC2, IC3, IC4, Latitude1, Latitude2, Latitude3, Latitude4, Longitude1, Longitude2, Longitude3, Longitude4) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        val = (date_string, time_string, Suhu1, Suhu2, Suhu3, Suhu4, RH1, RH2, RH3, RH4, SH1, SH2, SH3, SH4, IC1, IC2, IC3, IC4, Latitude1, Latitude2, Latitude3, Latitude4, Longitude1, Longitude2, Longitude3, Longitude4)
+        sql = "INSERT INTO monitoring_data (Tanggal, Waktu, Suhu1, Suhu2, Suhu3, Suhu4, RH1, RH2, RH3, RH4, SH1, SH2, SH3, SH4, IC1, IC2, IC3, IC4) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (date_string, time_string, Suhu1, Suhu2, Suhu3, Suhu4, RH1, RH2, RH3, RH4, SH1, SH2, SH3, SH4, IC1, IC2, IC3, IC4)
         print(val)
         cursor.execute(sql, val)
         
